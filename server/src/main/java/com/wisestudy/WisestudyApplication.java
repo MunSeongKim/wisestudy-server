d36e847897e7cc074b86fc485ea9a03a6650b8e2
munseongkim <chyin370@naver.com>
2020-06-09 01:13:35 +0900

    Spring Boot 테스트 코드 작성

---
+++ server/src/main/java/com/wisestudy/WisestudyApplication.java
@@ -0,0 +1,13 @@
+package com.wisestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WisestudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WisestudyApplication.class, args);
+	}
+
+}
