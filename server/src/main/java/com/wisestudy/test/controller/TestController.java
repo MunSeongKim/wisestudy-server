f6ff576272cf6e18914d7a09a6a2090832c3c302
munseongkim <chyin370@naver.com>
2020-06-09 04:00:41 +0900

    RequestMapping 변경, server.port 설정 누락 수정

--- server/src/main/java/com/wisestudy/test/controller/TestController.java
+++ server/src/main/java/com/wisestudy/test/controller/TestController.java
@@ -1,12 +1,10 @@
 package com.wisestudy.test.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-
     @GetMapping({"", "/", "/hello"})
     public String hello() {
         return "hello";
